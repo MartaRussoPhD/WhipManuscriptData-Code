16309a8c3d5b0327cfc5e70c20354c2958d40298
dondestamos <45499701+dondestamos@users.noreply.github.com>
2022-08-05 15:07:49 -0400

    Delete Dependencies directory

--- Dependencies/ColorModify.m
+++
@@ -1,7 +0,0 @@
-function colNew = ColorModify(colSource,H,S,L)
-%Modify color by HSL
-    tempCol = rgb2hsv(colSource);
-    tempCol = tempCol .* [H,S,L];
-    tempCol(tempCol>1) = 1;
-    colNew = hsv2rgb(tempCol);
-end
